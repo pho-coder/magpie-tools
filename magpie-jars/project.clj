be432c6ef3a1dcf39ef1e95f0e238ae20d0a6a80
Phoenix Zhang <pho.coderzhang@gmail.com>
2015-09-16 18:17:52 +0800

    fix

--- magpie-jars/project.clj
+++ magpie-jars/project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clj-zookeeper "0.2.0-SNAPSHOT"]
+                 [org.clojure/data.json "0.2.6"]]
   :main ^:skip-aot magpie-jars.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- magpie-jars/src/magpie_jars/core.clj
+++ magpie-jars/src/magpie_jars/core.clj
@@ -1,6 +1,19 @@
 (ns magpie-jars.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clj-zookeeper.zookeeper :as zk]
+            [clojure.data.json :as json]))
+
+(defn get-assignments-info
+  []
+  (let [assignments-path "/magpie/assignments"
+        children (zk/get-children assignments-path)]
+    (doseq [child children]
+      (prn (json (String. (zk/get-data (str assignments-path "/" child)))))))
 
 (defn -main
   [& args]
-  (println "Hello, World!"))
+  (println "Hi, magpie tools!")
+  (let [zk-str "192.168.144.107:2181,192.168.144.108:2181,192.168.144.109:2181"]
+    (zk/new-client zk-str)
+    (get-assignments-info)
+    (zk/close)))
